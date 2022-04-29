f5a148e796d8851f5079e41e263d3ae6f37d97d6
mdimransorker <apgimran@gmail.com>
2022-04-29 10:53:37 +0600

    app.js

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AboutMe from './Pages/AboutMe/AboutMe';
+import Blogs from './Pages/Blogs/Blogs';
+import CheckOut from './Pages/CheckOut/CheckOut';
+import Home from './Pages/Home/Home/Home';
+import LogIn from './Pages/LogIn/LogIn/LogIn';
+import RequireAuth from './Pages/LogIn/RequireAuth/RequireAuth';
+import SignUp from './Pages/LogIn/SignUP/SignUp';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth>
+            <CheckOut></CheckOut>
+          </RequireAuth>
+        }>
+        </Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<AboutMe></AboutMe>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </div >
+  );
+}
+
+export default App;
