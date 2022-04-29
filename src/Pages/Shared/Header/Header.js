cd899f001641092c2583763e56a5315683300456
mdimransorker <apgimran@gmail.com>
2022-04-29 10:55:48 +0600

    header

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,38 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+
+const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const logOut = () => {
+        signOut(auth)
+    }
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/home">Hermain Gym</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className='ms-auto'>
+                        <Nav.Link href="home#services">SERVICES</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">BLOGS</Nav.Link>
+                        <Nav.Link as={Link} to="/about">ABOUT</Nav.Link>
+                        {
+                            user
+                                ?
+                                <Nav.Link onClick={logOut}>LOG OUT</Nav.Link>
+                                :
+                                <Nav.Link as={Link} to="/login">LOGIN</Nav.Link>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
